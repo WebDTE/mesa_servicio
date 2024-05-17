823aabb5642e69125fa1452ba16d2b643fd76888
Gonzalo Torres <gonzalo.torres@prepaenlinea.sep.gob.mx>
2024-05-17 17:01:50 -0600

    news files

--- include/client/open.egresados.php
+++ include/client/open.egresados.php
@@ -4,9 +4,11 @@ if(!defined('OSTCLIENTINC'))
 
 $info=array();
 if($thisclient && $thisclient->isValid()) {
-    $info=array('name'=>$thisclient->getName(),
+    $info=array(
+                'name'=>$thisclient->getName(),
                 'email'=>$thisclient->getEmail(),
-                'phone'=>$thisclient->getPhoneNumber());
+                'phone'=>$thisclient->getPhoneNumber()
+            );
 }
 
 $info=($_POST && $errors)?Format::htmlchars($_POST):$info;
@@ -34,18 +36,19 @@ if ($info['topicId'] && ($topic=Topic::lookup($info['topicId']))) {
 
 ?>
 
-<h1><?php echo __('Abrir Ticket');?></h1>
+<h1><?php echo __('Egresados'); ?></h1>
+<h2><?php echo __('Abrir Ticket'); ?></h2>
 <p><?php echo __('Please fill in the form below to open a new ticket.');?></p>
 
 <form id="ticketForm" method="post" action="egresados.php" enctype="multipart/form-data">
     <?php csrf_token(); ?>
     <input type="hidden" name="a" value="open">
-    <table width="1000" cellpadding="1" cellspacing="0" border="0">
+    <table cellpadding="1" cellspacing="0" border="0"  class="mx-auto for-tabla">
         <tbody>
             <tr>
                 <td colspan="2"><hr />
-                    <div class="form-header" style="margin-bottom:0.5em"><b>
-                        <?php echo __('Help Topic'); ?></b></div>
+                   <!--  <div class="form-header" style="margin-bottom:0.5em"><b>
+                        <?php echo __('Help Topic'); ?></b></div> -->
                 </td>
             </tr>
             <tr>
@@ -88,7 +91,7 @@ if ($info['topicId'] && ($topic=Topic::lookup($info['topicId']))) {
                                 }
                             } 
                         ?>
-                    </select>
+                    </select><br>
                     <font class="error">&nbsp;<?php echo $errors['topicId']; ?></font>
                 </td>
             </tr>
@@ -133,9 +136,9 @@ if ($info['topicId'] && ($topic=Topic::lookup($info['topicId']))) {
             <tr class="captchaRow">
                 <td>
                     <span class="captcha"><img src="captcha.php" border="0" align="left"></span>
-                    <input id="captcha" type="text" name="captcha" size="6" autocomplete="off">
+                    <input id="captcha" type="text" name="captcha" size="6" autocomplete="off"><br>
                     <em><?php echo __('Enter the text shown on the image.');?></em>
-                    <font class="error">*&nbsp;<?php echo $errors['captcha']; ?></font>
+                    <!-- <font class="error">*&nbsp;<?php echo $errors['captcha']; ?></font> -->
                 </td>
             </tr>
             <?php
@@ -144,7 +147,23 @@ if ($info['topicId'] && ($topic=Topic::lookup($info['topicId']))) {
         </tbody>
     </table>
     <hr/>
-    <p class="buttons" style="text-align:center;">
+    <div class="row text-center justify-content-center">
+        <div class="col-12 col-md-8">
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-4 mb-3"><input type="submit" value="<?php echo __('Create Ticket'); ?>" class="btn btnh btn-mes-ser" onclick="return confirmEmail();"></div>
+                <div class="col-12 col-md-4 mb-3"><input type="reset" name="reset" value="<?php echo __('Reset'); ?>" class="btn btnh btn-mes-ser"></div>
+                <div class="col-12 col-md-4 mb-3"><input type="button" name="cancel" class="btn btnh btn-mes-ser" value="<?php echo __('Cancel'); ?>" onclick="javascript:
+                    $('.richtext').each(function() {
+                        var redactor = $(this).data('redactor');
+                        if (redactor && redactor.opts.draftDelete)
+                            redactor.plugin.draft.deleteDraft();
+                    });
+                    window.location.href='index.php';">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- <p class="buttons" style="text-align:center;">
           <input type="submit" value="<?php echo __('Create Ticket');?>" class="btn" onclick="return confirmEmail();">
           <input type="reset" name="reset" value="<?php echo __('Reset');?>" class="btn">
           <input type="button" name="cancel" class="btn" value="<?php echo __('Cancel'); ?>" onclick="javascript:
@@ -154,7 +173,7 @@ if ($info['topicId'] && ($topic=Topic::lookup($info['topicId']))) {
                       redactor.plugin.draft.deleteDraft();
               });
               window.location.href='index.php';">
-    </p>
+    </p> -->
 </form>
 
 <?php
